c5044986f941952f21ceb012e85b88b59e1c16e7
Rochak Pandey <rochakpandey56@gmail.com>
2023-01-18 21:35:38 +0530

    Implemented 'Transform your brand' and 'Stand out to right audience' sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
 import HeroSection from "./components/HeroSection";
+import TwoColumnView from "./components/TwoColumnView";
 
 function App() {
+  const twoColumnData = [
+    {
+      title: "Transform your brand",
+      description:
+        "We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you.",
+      imageUrl: "/images/desktop/image-transform.jpg",
+      orderReverse: true,
+    },
+    {
+      title: "Stand out to the right audience",
+      description:
+        "Using a collaborative formula of designers, researchers, photographers, videographers, and copywriters, we’ll build and extend your brand in digital places.",
+      imageUrl: "/images/desktop/image-stand-out.jpg",
+      orderReverse: false,
+    },
+  ];
   return (
     <div className="App">
       <HeroSection />
+      {twoColumnData.map((data, index) => (
+        <TwoColumnView key={index} {...data} />
+      ))}
     </div>
   );
 }
---
+++ src/components/TwoColumnView.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const TwoColumnView = (props) => {
+  // destructuring props
+  const { imageUrl, title, description, orderReverse } = props;
+
+  return (
+    <div className="grid w-full grid-cols-2">
+      <div className="col-span-2  md:col-span-1">
+        <img
+          src={`${process.env.PUBLIC_URL}${imageUrl}`}
+          alt=""
+          className="w-full object-cover"
+        />
+      </div>
+      <div
+        className={`col-span-2 flex flex-col flex-wrap content-center justify-center px-10 py-16 md:col-span-1 md:content-start md:px-28  md:py-0 ${
+          orderReverse ? "md:order-first" : ""
+        }`}
+      >
+        <h3 className="font-serif text-4xl font-extrabold">{title}</h3>
+        <p className="mt-8">{description}</p>
+        <button
+          className="learn-more-btn mt-8
+        font-serif font-extrabold uppercase
+        before:bg-primary-yellow"
+        >
+          Learn more
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default TwoColumnView;
