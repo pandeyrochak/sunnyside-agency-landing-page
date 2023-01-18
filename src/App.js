1507d4e872ede20d346650d86297292f51f1ac3d
Rochak Pandey <rochakpandey56@gmail.com>
2023-01-18 12:52:21 +0530

    Initial project setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-4xl font-bold text-center text-blue-700">Hello Tailwind!</h1>
+    </div>
+  );
+}
+
+export default App;
